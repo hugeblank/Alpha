a42c7df66d7c95727f9c2afd9c22105e739cfb5d
hugeblank <toddlangejr@gmail.com>
2018-01-02 10:35:10 -0800

    Update rules.lua

--- BetaBot/commands/rules.lua
+++ BetaBot/commands/rules.lua
@@ -1,2 +1,2 @@
 name = bagelBot.out()
-bagelBot.tell(name, {"&6*&rUse Common Sense", "&6*&rNo Griefing Claimed Land", "&6*&rYou are Not a Super Hacker, Don’t Hack", "&6*&rRespect All Players", "&6*&rNo Trolling/Flaming", "&6*&rJust Don’t Be Dumb", "&6*&rRespect Admins (people with pink (Or green) names)", "&6*&rOh and Have Fun!" })
+bagelBot.tell(name, {"&6*&rUse Common Sense", "&6*&rNo Griefing Claimed Land", "&6*&rYou are Not a Super Hacker, Don't Hack", "&6*&rRespect All Players", "&6*&rNo Trolling/Flaming", "&6*&rJust Don't Be Dumb", "&6*&rRespect Admins (people with pink (Or green) names)", "&6*&rNo Advertising", "&6*&rOh and Have Fun!" })
