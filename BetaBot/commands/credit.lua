6851319ab6a1dd30129ed215e62e6caf4bf95055
Todd Lange <toddlangejr@gmail.com>
2018-01-01 20:57:52 -0800

    Added more info to the credits

--- BetaBot/commands/credit.lua
+++ BetaBot/commands/credit.lua
@@ -1,1 +1,1 @@
-bagelBot.tell("@a", "View &6Beta&cBot&r at https://github.com/roger109z/BetaBot/")
+bagelBot.tell("@a", "View &6Beta&cBot&r at https://github.com/hugeblank/BetaBot/ Made by roger109z and adapted to BagelBot by hugeblank")
