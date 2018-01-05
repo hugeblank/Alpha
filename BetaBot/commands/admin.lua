293358efb8477f14936eec0d3308f792d3172e10
roger109z <roger_109g@yahoo.com>
2018-01-05 11:47:31 -0800

    Update admin.lua

--- BetaBot/commands/admin.lua
+++ BetaBot/commands/admin.lua
@@ -15,13 +15,13 @@ if args[1] == "set" then
 		bagelBot.tell(name, "No admins exist. Please confirm you are opped by going into the bot command computer and typing in your minecraft username.", false, betaBot.name)
 		local confirm = read()
 		if confirm == name then
-			print(betaBot.admindata)
-			bagelBot.setPersistence(betaBot.admindata, {name})
+			bagelBot.setPersistence(betaBot.levelData, {name})
 			print("Added "..name.."to admin list.")
 		end
 	end
 elseif args[1] == "list" then
 	bagelBot.tell(name, "Current Admins: ", false, betaBot.name)
+	if levels then
 	for k, v in pairs(levels) do
 		if tonumber(v) and tonumber(v) >= tonumber(betaBot.adminLevel) then
 			bagelBot.tell(name, k, true)
