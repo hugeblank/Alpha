407e7a8a9c74df10a6ad696fecf2cac33996832d
roger109z <roger_109g@yahoo.com>
2018-01-05 13:25:57 -0800

    Update firework.lua

--- BetaBot/commands/firework.lua
+++ BetaBot/commands/firework.lua
@@ -1,3 +1,5 @@
 local name = bagelBot.out()
-local x, y, z = betaBot.getPos(name)
-commands.summon("FireworksRocketEntity", x, y, z, "{LifeTime:20,FireworksItem:{id:fireworks,Count:1,tag:{Fireworks:{Explosions:[{Type:1,Flicker:1,Trail:1,Colors:[1182463],FadeColors:[16711680]}]}}}}")
+if isVIP(name) then
+  local x, y, z = betaBot.getPos(name)
+  commands.summon("FireworksRocketEntity", x, y, z, "{LifeTime:20,FireworksItem:{id:fireworks,Count:1,tag:{Fireworks:{Explosions:[{Type:1,Flicker:1,Trail:1,Colors:[1182463],FadeColors:[16711680]}]}}}}")
+end
