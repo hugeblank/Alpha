a028736fb1631f429b72353387c584849b6f217a
roger109z <roger_109g@yahoo.com>
2018-01-04 18:09:06 -0800

    Update tpahere.lua

--- BetaBot/commands/tpahere.lua
+++ BetaBot/commands/tpahere.lua
@@ -4,7 +4,7 @@ if type(betaBot.players[args[1]]) == "nil" then
     bagelBot.tell(name, "&cPlayer does not exist or is not online.", false, betaBot.name)
 end
 if args[1] ~= nil then
-    print(bagelBot.tell(args[1], name.." would like you to teleport to them, type or click: &6&g(!tpaccept "..name..")!tpaccept "..name.."&r to accept", false, betaBot.name))
+    print(bagelBot.tell(args[1], name.." would like you to teleport to them, type or click: &6&h(Click to Teleport)&g(!tpaccept "..name..")!tpaccept "..name.."&r to accept", false, betaBot.name))
     bagelBot.tell(name, "&eSent Teleport Request To: &6"..args[1], false, betaBot.name)
     if betaBot.tpList[args[1]] == nil then
         betaBot.tpList[args[1]] = {}
