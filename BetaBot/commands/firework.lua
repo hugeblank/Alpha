69993b92c99210ccf831e360c380c3f3caa558a8
hugeblank <toddlangejr@gmail.com>
2018-01-06 01:58:13 -0800

    goddamn it roger.

--- BetaBot/commands/firework.lua
+++ BetaBot/commands/firework.lua
@@ -1,5 +1,5 @@
 local name = bagelBot.out()
-if isVIP(name) then
+if betaBot.isVIP(name) then
   local x, y, z = betaBot.getPos(name)
   commands.summon("FireworksRocketEntity", x, y, z, "{LifeTime:20,FireworksItem:{id:fireworks,Count:1,tag:{Fireworks:{Explosions:[{Type:1,Flicker:1,Trail:1,Colors:[1182463],FadeColors:[16711680]}]}}}}")
 end
