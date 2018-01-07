e50881e87740aaf3347c322f1dd74b411cf9f341
hugeblank <toddlangejr@gmail.com>
2018-01-07 13:07:43 -0800

    Update restart command
    Notify everyone that the bot is restarting

--- BetaBot/commands/restart.lua
+++ BetaBot/commands/restart.lua
@@ -1,4 +1,5 @@
 local name = bagelBot.out()
 if betaBot.getLevel(name) >= betaBot.adminLevel then
+  bagelBot.tell("@a", "&6Restarting... Please hold all commands until I reboot...", false, betaBot.name)
   os.reboot()
 end
